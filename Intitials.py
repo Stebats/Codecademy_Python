03251690a234b2786e84d60a3e157deb4811bc75
Stebats <101889349+Stebats@users.noreply.github.com>
2022-06-25 23:04:55 +0100

    Initials.py
    ASCII Block Letters Project with python on Codecademy

---
+++ Intitials.py
@@ -0,0 +1,9 @@
+#Steven Bates
+#I am 18
+print (" SSS  BBBB ")
+print ("S   S B   B")
+print ("S     B   B")
+print (" SSS  BBBB ")
+print ("    S B   B")
+print ("S   S B   B")
+print (" SSS  BBBB " )
